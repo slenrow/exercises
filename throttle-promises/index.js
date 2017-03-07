fc4bc0c4a31b6d65b2ba90c1611cd653e4bd54e4
slenrow <stephen.lenrow@gmail.com>
2017-03-06 21:54:00 -0500

    Initialize throttle-promises

---
+++ throttle-promises/index.js
@@ -0,0 +1,10 @@
+/**
+ * Throttle promises implements a concurrency model limiting the execution of active parallel promises
+ * to a given limit.
+ *
+ * @param limit - The concurrency limit
+ * @param promises - The array of promises whose execution will be limited by `limit`
+ */
+module.exports = function throttlePromises(limit, promises) {
+
+}
