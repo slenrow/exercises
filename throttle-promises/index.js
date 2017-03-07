8bf6e18604dd0282863ae56ce5da28adde6426d8
slenrow <stephen.lenrow@gmail.com>
2017-03-07 00:23:00 -0500

    Create stubbed out function body

--- throttle-promises/index.js
+++ throttle-promises/index.js
@@ -1,10 +1,30 @@
 /**
- * Throttle promises implements a concurrency model limiting the execution of active parallel promises
+ * Throttle promises implements a concurrency model limiting the execution of active parallel functions as promises
  * to a given limit.
  *
  * @param limit - The concurrency limit
- * @param promises - The array of promises whose execution will be limited by `limit`
+ * @param actions - The array of actions whose execution will be limited by `limit`
  */
-module.exports = function throttlePromises(limit, promises) {
+module.exports = function throttlePromises(limit, actions) {
+    let index = 0; // Keep track of current index of actions being promisified
 
+    // Initialize array to store the results of the actions.
+    let results = [];
+
+    /*
+     * If there are still actions to be executed, increment `index` and promisify the action.
+     * This method will only be called iteratively by the outer scope a maximum of `limit` times.
+     */
+    const createPromise = () => {
+
+    }
+
+    // Initalize array to store the returned promises
+    let promises = [];
+
+    // Loop will call `createPromise` method until it reaches the concurrency limit
+    while (index < limit) {
+        let currentPromise = createPromise();
+        promises.push(currentPromise);
+    }
 }
